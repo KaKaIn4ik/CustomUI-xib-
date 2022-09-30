9f667caca1498c66a8fa578f4f48456734807832
e.kungurtsev <e.kungurtsev@2gis.ru>
2022-09-30 11:57:16 +0700

    Initial Commit

---
+++ CustomUI(xib)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomUI(xib)
+//
+//  Created by Кунгурцев Эдуард Сергеевич on 30.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
